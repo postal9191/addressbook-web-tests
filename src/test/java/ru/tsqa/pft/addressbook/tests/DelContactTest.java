d46efe61a9ea8f2ec4d81783b339385b293bbe35
postal9191 <postal.9191@mail.ru>
2022-11-06 21:41:47 +0300

    Задание №8 fix review №2

--- src/test/java/ru/tsqa/pft/addressbook/tests/DelContactTest.java
+++ src/test/java/ru/tsqa/pft/addressbook/tests/DelContactTest.java
@@ -1,7 +1,9 @@
 package ru.tsqa.pft.addressbook.tests;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import ru.tsqa.pft.addressbook.model.ContactData;
+import ru.tsqa.pft.addressbook.model.GroupData;
 
 public class DelContactTest extends TestBase {
 
@@ -11,6 +13,10 @@ public class DelContactTest extends TestBase {
                 "112", "9379992", "Mail@mail.ru", "test10");
 
         if (! app.getContactHelper().isThereAContact()){
+            app.getNavigationHelper().gotoGroupPage();
+            if (!app.isElementPresent(By.xpath(String.format("//*[@title='Select (%s)']", contact.getGroupName())))) {
+                app.getGroupHelper().createGroup(new GroupData("test10", "test2", "test3"));
+            }
             app.getContactHelper().addContact(contact);
         }
         app.getContactHelper().selectContact();

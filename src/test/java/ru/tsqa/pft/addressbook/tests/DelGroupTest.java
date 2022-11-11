c724aa2f97822963e070ffe0a84891db06e2bb98
postal9191 <postal.9191@mail.ru>
2022-11-11 21:22:03 +0300

    Лекция 4.5

--- src/test/java/ru/tsqa/pft/addressbook/tests/DelGroupTest.java
+++ src/test/java/ru/tsqa/pft/addressbook/tests/DelGroupTest.java
@@ -4,19 +4,21 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.tsqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class DelGroupTest extends TestBase {
 
     @Test
     public void testDelGroup() {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
         }
-        app.getGroupHelper().selectGroup(before - 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before - 1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() - 1);
     }
 }
--- src/test/java/ru/tsqa/pft/addressbook/tests/GroupModificationTests.java
+++ src/test/java/ru/tsqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,23 +4,25 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.tsqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
     @Test
-    public void testGroupModification(){
+    public void testGroupModification() {
         GroupData fillForm = new GroupData("test10", "test20", "test30");
 
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
-        if (! app.getGroupHelper().isThereAGroup()){
+        if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(fillForm);
         }
-        app.getGroupHelper().selectGroup(before - 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(fillForm);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
 }

19f2c41e2fcbb6735a990301f2eb2af92702aed1
postal9191 <postal.9191@mail.ru>
2022-11-11 21:06:19 +0300

    Лекция 4.4

--- src/test/java/ru/tsqa/pft/addressbook/tests/DelGroupTest.java
+++ src/test/java/ru/tsqa/pft/addressbook/tests/DelGroupTest.java
@@ -13,7 +13,7 @@ public class DelGroupTest extends TestBase {
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
         }
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnGroupPage();
         int after = app.getGroupHelper().getGroupCount();

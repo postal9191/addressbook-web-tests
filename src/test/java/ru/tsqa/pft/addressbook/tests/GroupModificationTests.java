3476f98ea3b1e9952cb30a78433abebc793a724f
postal9191 <postal.9191@mail.ru>
2022-11-13 19:28:16 +0300

    Лекция 4.7

--- src/test/java/ru/tsqa/pft/addressbook/tests/GroupModificationTests.java
+++ src/test/java/ru/tsqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,25 +4,30 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.tsqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
 
     @Test
     public void testGroupModification() {
-        GroupData fillForm = new GroupData("test10", "test20", "test30");
 
         app.getNavigationHelper().gotoGroupPage();
         if (!app.getGroupHelper().isThereAGroup()) {
-            app.getGroupHelper().createGroup(fillForm);
+            app.getGroupHelper().createGroup(new GroupData("test10", "test20", "test30"));
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
+        GroupData fillForm = new GroupData(before.get(before.size() - 1).getId(),"test10", "test20", "test30");
         app.getGroupHelper().fillGroupForm(fillForm);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(fillForm);
+        Assert.assertEquals(new HashSet<Object>(before) , new HashSet<Object>(after));
     }
 }

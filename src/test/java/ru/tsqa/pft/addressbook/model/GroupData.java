9142c4e9a4e7854933d52112cc681fbca2a84ebc
postal9191 <postal.9191@mail.ru>
2022-11-18 00:22:35 +0300

    Лекция 5.5

--- src/test/java/ru/tsqa/pft/addressbook/model/GroupData.java
+++ src/test/java/ru/tsqa/pft/addressbook/model/GroupData.java
@@ -13,7 +13,15 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
+        if (id != groupData.id) return false;
         return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 
     public GroupData withId(int id) {
@@ -34,11 +42,6 @@ public class GroupData {
     public GroupData withFooter(String footer) {
         this.footer = footer;
         return this;
-    }
-
-    @Override
-    public int hashCode() {
-        return name != null ? name.hashCode() : 0;
     }
 
     public GroupData(int id, String name, String header, String footer) {

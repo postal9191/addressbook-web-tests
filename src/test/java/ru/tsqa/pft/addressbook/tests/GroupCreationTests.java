73035221de0a1b17ba5786dbdfaec941e794d2fd
postal9191 <postal.9191@mail.ru>
2022-11-25 23:24:55 +0300

    Лекция 6.4

--- src/test/java/ru/tsqa/pft/addressbook/tests/GroupCreationTests.java
+++ src/test/java/ru/tsqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,19 +1,33 @@
 package ru.tsqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.tsqa.pft.addressbook.model.GroupData;
 import ru.tsqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{new GroupData().withName("test1").withHeader("test1").withFooter("test1")});
+        list.add(new Object[]{new GroupData().withName("test2").withHeader("test2").withFooter("test2")});
+        list.add(new Object[]{new GroupData().withName("test3").withHeader("test3").withFooter("test3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData("test1", "test2", "test3");
+
         app.group().create(group);
 
         assertThat(app.group().count(), equalTo(before.size() + 1));

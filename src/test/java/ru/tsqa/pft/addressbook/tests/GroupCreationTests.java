84ffe1b8a55dd2f39adfd2c44bb08e0925272789
postal9191 <postal.9191@mail.ru>
2022-11-26 13:32:22 +0300

    Лекция 6.6

--- src/test/java/ru/tsqa/pft/addressbook/tests/GroupCreationTests.java
+++ src/test/java/ru/tsqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,18 @@
 package ru.tsqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.tsqa.pft.addressbook.model.GroupData;
 import ru.tsqa.pft.addressbook.model.Groups;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
@@ -17,16 +21,20 @@ public class GroupCreationTests extends TestBase {
 
     @DataProvider
     public Iterator<Object[]> validGroups() throws IOException {
-        List<Object[]> list = new ArrayList<Object[]>();
+        //List<Object[]> list = new ArrayList<Object[]>();
         //list.add(new Object[]{new GroupData().withName("test1").withHeader("test1").withFooter("test1")});
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+        String xml = "";
         String line = reader.readLine();
-        while (line != null){
-            String[] split = line.split(";");
-            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+        while (line != null) {
+            xml += line;
             line = reader.readLine();
         }
-        return list.iterator();
+        XStream xstream = new XStream();
+        xstream.processAnnotations(GroupData.class);
+        xstream.allowTypes(new Class[]{GroupData.class});// в лекции данной строки нет, но без нее не работают тесты
+        List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+        return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
 
     @Test(dataProvider = "validGroups")

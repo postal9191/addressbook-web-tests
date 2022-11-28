453cdd0da42d2be7e87286aafd1b684e9fbd7461
postal9191 <postal.9191@mail.ru>
2022-11-28 17:00:17 +0300

    Лекция 6.10 = Задание №14

--- src/test/java/ru/tsqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/tsqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -5,42 +5,53 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class ApplicationManager {
-    private final Browser browser;
+
     WebDriver wd;
 
+    private String browser;
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private final Properties properties;
 
-    public ApplicationManager(Browser browser) {
+    public ApplicationManager(String browser){
         this.browser = browser;
+        properties = new Properties();
+
     }
 
-    public void init() {
-        if (browser.equals(Browser.CHROME)) {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+
+        if (browser.equals(BrowserType.CHROME)) {
             System.setProperty("webdriver.chrome.driver", "c:\\chromedriver\\105\\chromedriver.exe");
             wd = new ChromeDriver();
-        } else if (browser.equals(Browser.FIREFOX)) {
+        } else if (browser.equals(BrowserType.FIREFOX)) {
             System.setProperty("webdriver.gecko.driver", "c:\\chromedriver\\geckodriver.exe");
             wd = new FirefoxDriver();
-        } else if (browser.equals(Browser.OPERA)) {
+        } else if (browser.equals(BrowserType.OPERA)) {
             System.setProperty("webdriver.chrome.driver", "c:\\chromedriver\\operadriver.exe");
             wd = new ChromeDriver();//построен на базе Хромиум
         }
 
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
-        wd.get("http://localhost/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
         contactHelper = new ContactHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
--- src/test/java/ru/tsqa/pft/addressbook/tests/TestBase.java
+++ src/test/java/ru/tsqa/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,13 @@
 package ru.tsqa.pft.addressbook.tests;
 
-import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.tsqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    static ApplicationManager app = new ApplicationManager(Browser.CHROME);
+    static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp() throws Exception {

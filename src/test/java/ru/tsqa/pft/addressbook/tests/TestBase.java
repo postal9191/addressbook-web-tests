8f47e7af3bd8a45b274c1c05452f6364765dc278
postal9191 <postal.9191@mail.ru>
2022-11-30 09:57:10 +0300

    Лекция 6.11

--- src/test/java/ru/tsqa/pft/addressbook/tests/TestBase.java
+++ src/test/java/ru/tsqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,19 @@
 package ru.tsqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.tsqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -14,10 +22,13 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
     }
 
-
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p){
+        logger.info("Start test " + m.getName() + Arrays.asList(p));
+    }
 }

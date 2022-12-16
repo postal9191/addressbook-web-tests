37cf9b298fa2764ff0b9dd0a7be391b6a042f338
postal9191 <postal.9191@mail.ru>
2022-12-16 17:31:52 +0300

    Лекция 10.2 реализована возможность запуска браузера

--- src/test/java/ru/tsqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/tsqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,10 +6,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.Properties;
 
@@ -36,16 +39,23 @@ public class ApplicationManager {
 
         dbHelper = new DbHelper();
 
-        if (browser.equals(BrowserType.CHROME)) {
-            System.setProperty("webdriver.chrome.driver", "c:\\chromedriver\\107\\chromedriver.exe");
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.FIREFOX)) {
-            System.setProperty("webdriver.gecko.driver", "c:\\chromedriver\\geckodriver.exe");
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.OPERA)) {
-            System.setProperty("webdriver.chrome.driver", "c:\\chromedriver\\operadriver.exe");
-            wd = new ChromeDriver();//построен на базе Хромиум
+        if("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(BrowserType.CHROME)) {
+                System.setProperty("webdriver.chrome.driver", "c:\\chromedriver\\107\\chromedriver.exe");
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.FIREFOX)) {
+                System.setProperty("webdriver.gecko.driver", "c:\\chromedriver\\geckodriver.exe");
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.OPERA)) {
+                System.setProperty("webdriver.chrome.driver", "c:\\chromedriver\\operadriver.exe");
+                wd = new ChromeDriver();//построен на базе Хромиум
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
+
 
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
         wd.get(properties.getProperty("web.baseUrl"));
